324345c3bd2d28ca623bcc6b68fa1e64c5fe93bc
daaoooy <y020d226y@daum.net>
2025-11-30 01:48:35 +0900

    ✨ (#6) 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
+import { AppRouter } from "@/app/routes/AppRouter";
+
 function App() {
-  return (
-    <>
-      <div className="flex justify-center items-center h-full gap-5">
-        <div className="bg-boost-blue text-white">색상 토큰 테스트</div>
-        <div className="title1-bold">폰트 토큰 테스트</div>
-      </div>
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
