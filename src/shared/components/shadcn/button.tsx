861be6640e309f6e823c3d4a1519ac6e937014a5
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2025-11-30 02:07:50 +0900

    ✨ (#9, #16) 라우팅 정의 + 사이드바 구현 (#17)
    * 🔥 (#16) 기존 페이지들 삭제
    
    * 📦(#9, #16) shadcn 컴포넌트들, react-router-dom 설치
    
    * 📦(#9) shadcn 컴포넌트들 설치
    
    * 🍱 (#9) boost icon assets 추가
    
    * ✨ (#9) variants에 boost 버튼 디자인 추가
    
    * ✨ (#9) 사이드바 구현
    
    * ✨(#16) 라우팅 정의
    
    * ✨ (#6) 레이아웃 적용
    
    * ✨ (#9, #16) 필요한 페이지들 추가
    
    * 🚨 shadcn 컴포넌트에서 생긴 린트 오류 무시

--- src/shared/components/shadcn/button.tsx
+++ src/shared/components/shadcn/button.tsx
@@ -12,6 +12,10 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        boostBlue:
+          "bg-boost-blue hover:bg-boost-blue-hover cursor-pointer text-primary-foreground duration-300",
+        boostOrange:
+          "bg-boost-orange hover:bg-boost-orange-hover cursor-pointer text-primary-foreground duration-300",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
